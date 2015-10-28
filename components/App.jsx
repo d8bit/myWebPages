6199cd313641804d70474a3d5b1f3ba1a28f6e8a
Deibit <sickboy140@hotmail.com>
2015-10-28 23:05:33 +0100

    Removed useless line

--- components/App.jsx
+++ components/App.jsx
@@ -4,7 +4,6 @@ var App = React.createClass({
   },
   updateFilter: function (filterValue) {
       this.setState({filterInput: filterValue});
-      this.refs.appGroup.props.filter = filterValue;
   },
   render: function() {
       var appsList = [];
