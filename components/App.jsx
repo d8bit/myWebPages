6f9560c1bdb0e8dc2fcf48b932860d691d24d5a4
Deibit <sickboy140@hotmail.com>
2016-01-17 13:18:50 +0100

    Refactor. Added ajax call to iterate over htdocs directory

--- components/App.jsx
+++ components/App.jsx
@@ -1,33 +1,50 @@
+/* Application */
+var directories = '';
 var App = React.createClass({
-  getInitialState: function() {
-      return { filterInput: '' };
-  },
-  updateFilter: function (filterValue) {
-      this.setState({filterInput: filterValue});
-  },
-  render: function() {
-      var appsList = [];
-      for (var i = 0; i < 1000; i++) {
-          appsList.push({name: "app " + i, link: "#"});
-      }
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-lg-12 head">
-                    <h1>My apps</h1>
+    getInitialState: function() {
+        return {
+            directories: '',
+            filterInput: ''
+        };
+    },
+    /* Load ajax data */
+    componentDidMount: function() {
+        $.get('directories.php', function(result) {
+            if (this.isMounted()) {
+                result = JSON.parse(result);
+                this.setState({
+                    directories: result
+                });
+            }
+        }.bind(this));
+    },
+    updateFilter: function (filterValue) {
+        this.setState({filterInput: filterValue});
+    },
+    render: function() {
+        var appsList = [];
+        console.log(this.state);
+        for (i = 0; i < this.state.directories.length; i++) {
+            appsList.push({name: this.state.directories[i], link: "http://localhost/" + this.state.directories[i]});
+        }
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-12 head">
+                        <h1>My apps</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <SearchForm applyFilter={this.updateFilter} />
+                </div>
+                <div className="row">
+                    <div className="col-lg-12">
+                       <AppGroup ref="appGroup" apps={appsList} filter={this.state.filterInput} />
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                  <SearchForm applyFilter={this.updateFilter} />
-            </div>
-            <div className="row">
-                <div className="col-lg-12">
-                   <AppGroup ref="appGroup" apps={appsList} filter={this.state.filterInput} />
-                </div>
-            </div>
-        </div>
-    );
-  }
+        );
+    }
 });
 
 ReactDOM.render(
---
+++ directories.php
@@ -0,0 +1,15 @@
+<?php
+
+$folder = '/Applications/XAMPP/htdocs/';
+
+$flags = FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::SKIP_DOTS | FilesystemIterator::UNIX_PATHS;
+$d_iterator = new RecursiveDirectoryIterator($folder, $flags);
+
+$directories = array();
+foreach ($d_iterator as $file) {
+    if ($d_iterator->isDir()) {
+        $directories[] = $d_iterator->getFileName();
+    }
+}
+
+echo json_encode($directories);
