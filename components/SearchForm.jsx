2fd8c7f1a1c5867322f408e9644ec2fdc9b1f964
Deibit <sickboy140@hotmail.com>
2016-01-21 21:05:53 +0100

    Minor changes

--- components/SearchForm.jsx
+++ components/SearchForm.jsx
@@ -5,7 +5,7 @@ var SearchForm = React.createClass({
     render: function() {
       return (
           <div>
-              <div className="col-lg-12">
+              <div className="col-lg-6">
                   <form action="https://www.google.es/search" method="GET" target="_blank">
                       <div className="form-group">
                           <label htmlFor="googleSearcher">Google</label>
@@ -14,7 +14,7 @@ var SearchForm = React.createClass({
                   </form>
               </div>
 
-              <div className="col-lg-12">
+              <div className="col-lg-6">
                   <form action="http://www.stackoverflow.com/search" method="GET" target="_blank">
                       <div className="form-group">
                           <label htmlFor="stackSearcher">Stack Overflow</label>
@@ -22,7 +22,7 @@ var SearchForm = React.createClass({
                       </div>
                   </form>
               </div>
-              <div className="col-lg-12">
+              <div className="col-lg-6">
                   <form action="" method="POST">
                       <div className="form-group">
                           <label htmlFor="search">Search</label>
